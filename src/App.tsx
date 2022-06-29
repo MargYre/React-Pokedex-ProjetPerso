7acc191da905b6301e100dc11292a4fb9b53f50f
Tarayre Margaux <tmargaux@desktop-mpp46st.home>
2022-06-29 15:16:57 +0200

    Add: display of all pokemon cards (name, id, picture, date of creation)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
+import Pokemon from "./models/pokemon";
+import POKEMONS from "./models/mock-pokemon";
 
 const App: FunctionComponent = () => {
-    const name: String = 'React';
-       
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+    useEffect(() => {
+        setPokemons(POKEMONS); 
+    }, []);
+
     return (
-     <h1>Hello, {name} !</h1>
+        <div>
+            <h1 className="center">Pokédex</h1>
+            <div className="container">
+                <p className="center"> Il y a {pokemons.length} pokémons dans le Pokédex.</p>
+                <div className="row">
+                    {pokemons.map(({id, name, picture, created})=> (
+                        <div className="col s6 m4" key={id}>
+                            <div className="card horizontal">
+                                <div className="card-image">
+                                    <img src={picture} alt={name}/>
+                                </div>
+                                <div className="card-stacked">
+                                    <div className="card-content">
+                                        <p> <b> nom:</b> {name}</p>
+                                        <p><b>id:</b> {id}</p>
+                                        <p><b>date de creation:</b> {created.toString()}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+        </div>
     )
    }
      
