e5e5dd42fba522474bf9338fff44453c6cbfdf35
Tarayre Margaux <tmargaux@localhost.localdomain>
2022-06-30 17:04:22 +0200

    ADD: creation of pokemon-card component and the color of the boder turn pink when the mouse is on a card

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,12 @@
-import React, { FunctionComponent, useState, useEffect } from "react";
+import React, { FunctionComponent } from "react";
 import Pokemon from "./models/pokemon";
 import POKEMONS from "./models/mock-pokemon";
+import PokemonList from "./pages/pokemon-list";
 
 const App: FunctionComponent = () => {
-    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
-    useEffect(() => {
-        setPokemons(POKEMONS); 
-    }, []);
 
     return (
-        <div>
-            <h1 className="center">Pokédex</h1>
-            <div className="container">
-                <p className="center"> Il y a {pokemons.length} pokémons dans le Pokédex.</p>
-                <div className="row">
-                    {pokemons.map(({id, name, picture, created})=> (
-                        <div className="col s6 m4" key={id}>
-                            <div className="card horizontal">
-                                <div className="card-image">
-                                    <img src={picture} alt={name}/>
-                                </div>
-                                <div className="card-stacked">
-                                    <div className="card-content">
-                                        <p> <b> nom:</b> {name}</p>
-                                        <p><b>id:</b> {id}</p>
-                                        <p><b>date de creation:</b> {created.toString()}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-
-            </div>
-        </div>
+        <PokemonList/>
     )
    }
      
