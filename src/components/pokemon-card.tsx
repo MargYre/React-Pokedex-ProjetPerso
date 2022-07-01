8c6cee7544e6280fbfd2c6c45725d50257d94306
Tarayre Margaux <tmargaux@desktop-mpp46st.home>
2022-07-01 11:52:51 +0200

    Add helpers directory and types of pokémon are display in the web app

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent, useState } from "react";
 import Pokemon from "../models/pokemon";
 import './pokemon-card.css';
+import formatDate from "../helpers/format-date";
+import formatType from "../helpers/format-type";
 
 type Props = {
     pokemon: Pokemon,
@@ -17,7 +19,7 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'
 
     const hideBorder = () => {
         setColor('#f5f5f5'); //On remet la bordures en gris
-    }
+    }    
 
     return (
         <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
@@ -27,8 +29,11 @@ const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'
                 </div>
                     <div className="card-stacked">
                     <div className="card-content">
-                         <p>{pokemon.name}</p>
-                        <p><small>{pokemon.created.toString()}</small></p>
+                        <p>{pokemon.name}</p>
+                        <p><small>{formatDate(pokemon.created)}</small></p>
+                        {pokemon.types.map(type => (
+                            <span key={type} className={formatType(type)}>{type}</span>
+                        ))}
                     </div>
                 </div>
             </div> 
